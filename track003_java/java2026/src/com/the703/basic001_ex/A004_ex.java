59bb81be67a1466d42d954f9e9e829da4d084e84
mundaewon1 <less40@naver.com>
2026-03-27 18:04:20 +0900

    day002-정리

---
+++ track003_java/java2026/src/com/the703/basic001_ex/A004_ex.java
@@ -0,0 +1,10 @@
+package com.the703.basic001_ex;
+
+public class A004_ex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("도시 : %s , 인구:%d" , "인천" , 1000000 );
+	}
+
+}
