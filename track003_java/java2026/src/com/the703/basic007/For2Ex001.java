e40d33b5a23c84a2a73a9caf1b9c6cc0747b3f86
mundaewon1 <less40@naver.com>
2026-04-11 23:35:58 +0900

    d

--- track003_java/java2026/src/com/the703/basic007/For2Ex001.java
+++ track003_java/java2026/src/com/the703/basic007/For2Ex001.java
@@ -3,22 +3,30 @@ package com.the703.basic007;
 public class For2Ex001 {
 
 	public static void main(String[] args) {
-		//ver-1 눈에 보이는 그대로
-		System.out.print('#'); System.out.print('#'); System.out.print('#'); System.out.print('#');System.out.println();
-		System.out.print('#'); System.out.print('#'); System.out.print('#'); System.out.print('#');System.out.println();
-		System.out.print('#'); System.out.print('#'); System.out.print('#'); System.out.print('#');System.out.println();
-		System.out.print('#'); System.out.print('#'); System.out.print('#'); System.out.print('#');System.out.println();
-		//ver-2 칸정리
+		System.out.print("★"); System.out.print("★"); System.out.print("★"); System.out.print("★"); System.out.println();
+		System.out.print("★"); System.out.print("★"); System.out.print("★"); System.out.print("★"); System.out.println();
+		System.out.print("★"); System.out.print("★"); System.out.print("★"); System.out.print("★"); System.out.println();
+		System.out.print("★"); System.out.print("★"); System.out.print("★"); System.out.print("★"); System.out.println();
 		System.out.println();
-		
-		for(int i=1;i<=4;i++){System.out.print('#');}System.out.println();
-		//ver-3 층정리
-		for(int a=1;a<=4;a++)
-		
-		{for(int i=1;i<=4;i++){System.out.print('#');}System.out.println();}
-
-		
-		
+		for(int i=1;i<=4;i++){System.out.print("★");}System.out.println();
+System.out.println();
+		for(int b=1;b<=4;b++)
+		{for(int i=1;i<=4;i++){System.out.print("★");}System.out.println();}
 	}
 
 }
+//ver-1 눈에 보이는 그대로
+//System.out.print('#'); System.out.print('#'); System.out.print('#'); System.out.print('#');System.out.println();
+//System.out.print('#'); System.out.print('#'); System.out.print('#'); System.out.print('#');System.out.println();
+//System.out.print('#'); System.out.print('#'); System.out.print('#'); System.out.print('#');System.out.println();
+//System.out.print('#'); System.out.print('#'); System.out.print('#'); System.out.print('#');System.out.println();
+////ver-2 칸정리
+//System.out.println();
+//
+//for(int i=1;i<=4;i++){System.out.print('#');}System.out.println();
+////ver-3 층정리
+//for(int a=1;a<=4;a++)
+//
+//{for(int i=1;i<=4;i++){System.out.print('#');}System.out.println();}
+//
+
