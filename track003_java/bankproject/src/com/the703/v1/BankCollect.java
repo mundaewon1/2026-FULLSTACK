f28c80805b25854b160455664568c38a5fdce0c1
mundaewon1 <less40@naver.com>
2026-05-15 18:16:34 +0900

    day

--- track003_java/bankproject/src/com/the703/v1/BankCollect.java
+++ track003_java/bankproject/src/com/the703/v1/BankCollect.java
@@ -3,37 +3,30 @@ package com.the703.v1;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Scanner;
 
-//1. Dto 데이터전송목적
+//1. Dto 데이터전송목적 ( 기본생성자, 필드생성자, toString, getters/setters, hashCode/equals)
 class BankDto{
 	private String id;
 	private String pass;
 	private double balance;
 	
 	
-	public BankDto() { super(); }
+	public BankDto() {  }
 	public BankDto(String id, String pass, double balance) { super(); this.id = id; this.pass = pass; this.balance = balance; }
 	 @Override public String toString() { return "BankDto [id=" + id + ", pass=" + pass + ", balance=" + balance + "]"; }
 	 
 	 public String getId() { return id; }  public void setId(String id) { this.id = id; }
 	 public String getPass() { return pass; }  public void setPass(String pass) { this.pass = pass; }
 	 public double getBalance() { return balance; }  public void setBalance(double balance) { this.balance = balance; }
+	 
+	 @Override public int hashCode() { return Objects.hash(id); }
+	 @Override public boolean equals(Object obj) { if (this == obj) return true; if (obj == null) return false; if (getClass() != obj.getClass()) return false; BankDto other = (BankDto) obj; return Objects.equals(id, other.id); }
 
-	@Override public int hashCode() { return Objects.hash(balance, id, pass); }
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		BankDto other = (BankDto) obj;
-		return Double.doubleToLongBits(balance) == Double.doubleToLongBits(other.balance)
-				&& Objects.equals(id, other.id) && Objects.equals(pass, other.pass);
-	}
-}
 
+}// end BankDto
+
+//2. 기능클래스
 class Bank{
 	List<BankDto>  users;   // 객체를 생성하는게 아니라 정보만 받을 목적
 	
@@ -41,19 +34,84 @@ class Bank{
 	public Bank(List<BankDto> users) { super(); this.users = users; }
 	
 	// 메뉴 - 안에 내용작성
-	public void menu() {}   
-	// 유저추가  (add)
+	public void menu() {
+		int menu=-1;
+		Scanner sc = new Scanner(System.in);
+		
+		while(menu != 9) {
+	        System.out.print("\n\n🌟💰 WELCOME TO BANK SYSTEM 💰🌟\r\n"
+	              + "[1] ➕ 계좌 추가 [2] 🔍 계좌 조회 [3] 💵 입금하기 [4] 💸 출금하기 [5] 🗑️ 계좌 삭제  [9]종료\r\n"
+	              + "👉 번호를 선택하세요:");
+	        menu = sc.nextInt();
+	        
+	        if( menu == 1 ) {  add(); }
+	        else {
+	        	BankDto find = login();// 로그인확인
+	        	if( find == null ) { System.out.println("정보를 확인해주세요"); continue; }
+	        	// 각각의 메뉴에 맞는 기능호출
+	        	switch(menu) {
+	        		case 2 : 	show(find);  break;
+	        		case 3 : 	deposit(find);  break;
+	        		case 4 : 	withdraw(find);  break;
+	        		case 5 : 	delete(find);  break;
+	        		case 9 : 	exit();  break;
+	        	}
+	        }
+		}
+	}   
+	// 유저추가  (add) 입금 출금 유저삭제 종료
 	public void add() {
 		//변수
+		Scanner sc = new Scanner(System.in);
 		//입력 - 사용자에게 정보입력받기
-		//처리 
-		users.add( new BankDto("aaa" , "pass" , 1 ) );
+		System.out.println("아이디  		입력 > "); String tempid = sc.next();  // 아이디중복검사
+		if( users.contains(tempid)==true ) {System.out.println("중복되는 아이디입니다"); return;}
+		
+		System.out.println("비밀번호  	입력 > "); String temppass = sc.next();
+		System.out.println("잔액			입력 > "); double tempbalance = sc.nextDouble();
+		
+		//처리 list -> add, get, size, remove, contains
+		users.add( new BankDto(tempid, temppass , tempbalance ) );
+		System.out.println(users);
 		//출력
+		
 	}
-	// 입금   (get)
-	// 출금   (get)
-	// 유저삭제(remove)
-	// 종료   
+	// 유저로그인		  - 유저정보 BankDto login(){}
+	BankDto login() { 
+		//변수
+		Scanner sc = new Scanner(System.in);
+		//입력 - 사용자에게 정보입력받기
+		System.out.println("아이디  		입력 > "); String tempid = sc.next();  // 아이디중복검사
+		System.out.println("비밀번호  	입력 > "); String temppass = sc.next();
+		//처리
+		for(BankDto u : users) {
+			if( u.getId().equals(tempid) && u.getPass().equals(temppass) ) { return u; }
+		}
+		return null; 
+	}
+	
+    void     show( BankDto  user  ){
+      System.out.printf("ID : %s\nPASS: %s\nBALANCE: %.1f\n" ,user.getId(),user.getPass(), user.getBalance()); 
+	} 
+	// 입금   (get)	  - 		  
+	void	  deposit( BankDto user ){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("입금할 금액 입력 > "); double tempbalance = sc.nextDouble();
+		user.setBalance( user.getBalance()+ tempbalance );
+		System.out.println("입금완료!");
+	}
+	// 출금   (get)	  - 		  
+	void	  withdraw( BankDto user ){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("출금할 금액 입력 > "); double tempbalance = sc.nextDouble();
+		if( user.getBalance() < tempbalance ) { System.out.println("잔액이 모자랍니다."); return;}
+		user.setBalance( user.getBalance()+ tempbalance );
+		System.out.println("출금완료!");
+	}
+	// 유저삭제(remove) -  		  
+	void	  delete ( BankDto user ){ System.out.println( users.remove(user) ? "유저삭제완료":"관리자문의");}
+	// 종료   		  - 		  
+	void	  exit	(  ){}
 }
 
 public class BankCollect {
