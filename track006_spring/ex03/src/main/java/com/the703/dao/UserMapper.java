a6a2b6d906ff4614f9105619e0ac013030dde6ef
mundaewon1 <less40@naver.com>
2026-06-09 18:20:50 +0900

    day

---
+++ track006_spring/ex03/src/main/java/com/the703/dao/UserMapper.java
@@ -0,0 +1,35 @@
+package com.the703.dao;
+
+import com.the703.dto.AuthDto;
+import com.the703.dto.AuthListDto;
+import com.the703.dto.UserDto;
+
+@Mapper
+public interface UserMapper {
+	public int     insert(UserDto dto);
+	public int     findLogin(UserDto dto);
+	public UserDto findByUno(int uno);
+	public String  findByEmail(String email);
+	
+	/* security */
+	public int         insertAuth(AuthDto dto);
+	public AuthListDto readAuth(AuthDto dto);
+}
+
+/*
+create : 
+    insert into users (nickname, bpass, email, mobile, bip)
+    values            (#{nickname},#{bpass},#{email},#{mobile},#{bip})
+
+read : 
+    select count(*) from users where email=#{email} and bpass=#{bpass}
+    select       *  from users where uno=#{uno}
+    select    email from users where email=#{email}
+    
+    
+    insert into authorities (email, auth) values (#{email},#{auth})
+    
+    select  u.email, u.bpass, a.auth
+    from    users u left join authorities a  on u.email = a.email
+    where   u.email = #{email}
+ */
