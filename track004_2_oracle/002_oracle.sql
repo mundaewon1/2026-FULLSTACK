7e03167345539f331f2a0313a9e95410c32e13ed
mundaewon1 <less40@naver.com>
2026-06-25 18:19:25 +0900

    d

--- track004_2_oracle/002_oracle.sql
+++ track004_2_oracle/002_oracle.sql
@@ -38,6 +38,23 @@ insert into sboard2 ( ID                 , APP_USER_ID, BTITLE, BCONTENT, BPASS,
             values  ( sboard2_seq.nextval, 1001,'title','bcontent','1111','1.png', '127.0.0.1' );
 전체(페이징) 
 select * from sboard2 order by id desc;
+
+SELECT * FROM sboard2 
+ORDER BY id DESC
+OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY;
+
+--  OFFSET 0 ROWS: 건너뛸 행의 개수 (0부터 시작)
+--  FETCH NEXT 10 ROWS ONLY: 가져올 행의 개수
+
+select count(*) from sboard2;
+-- 11g 이하
+select * from(
+         select row_number() over(order by id desc)  as rnum, 
+                id, app_user_id, btitle, bcontent, bpass, bfile, bip, bhit, created_at
+         from  sboard2
+      ) A  
+      where  A.rnum  between  0  and 10;
+
 해당번호의 
 select * from sboard2 where id=1;
 해당번호 조회수 올리기 
@@ -47,7 +64,7 @@ update sboard2 set btitle='new', bcontent='new', bfile='2.png' where id=2;
 해당번호 삭제 
 delete from sboard2 where id=1;
 
-
-
+INSERT INTO sboard2 (ID, APP_USER_ID, BTITLE, BCONTENT, BPASS, BIP, CREATED_AT) SELECT (SELECT NVL(MAX(ID), 0) FROM sboard2) + ROWNUM, APP_USER_ID, BTITLE, BCONTENT, BPASS, BIP, SYSDATE FROM sboard2;
+commit;
 2) sboard2-mapper.xml
 3) Sboard2Dao.java
--- track007_boot_api/boot1/src/main/java/com/the703/controller/BasicController.java
+++ track007_boot_api/boot1/src/main/java/com/the703/controller/BasicController.java
@@ -1,6 +1,9 @@
 package com.the703.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,4 +13,16 @@ public class BasicController {
 	//@RequestMapping("/tmpt")
 	@GetMapping("/tmpt")
 	public String tmpt() { return "tmpt"; }  // prefix(/templates)  +  tmpt  +  suffix(.html)
+	
+	
+	@GetMapping("/basic")
+	public String basic( Model model) { 
+		model.addAttribute("msg", "hello");
+		model.addAttribute("nums", List.of(1,2,3,4,5) );
+		return "basic"; 
+	}
+	
+	//1. tmpt 복사해서 basic 만들기
+	//2. http://localhost:8080/basic 했을때 basic.html 열리기
+	
 }
