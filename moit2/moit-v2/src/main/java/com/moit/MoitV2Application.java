0bdf40695f709c1bc856253a483c2b8195320874
mundaewon1 <less40@naver.com>
2026-07-06 12:09:53 +0900

    d

---
+++ moit2/moit-v2/src/main/java/com/moit/MoitV2Application.java
@@ -0,0 +1,13 @@
+package com.moit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoitV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoitV2Application.class, args);
+	}
+
+}
