e1c0ace4dbf2a3dc053595ca4ced98e46b711f32
mundaewon1 <less40@naver.com>
2026-07-06 12:44:39 +0900

    d

--- moit2/moit-v1/src/main/java/com/moit/controller/NoticeController.java
+++
@@ -1,8 +0,0 @@
-package com.moit.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class NoticeController {
-
-}
--- moit2/moit-v1/src/main/java/com/moit/dto/AuthDto.java
+++
@@ -1,9 +0,0 @@
-package com.moit.dto;
-
-import lombok.Data;
-
-@Data
-public class AuthDto {
-	private String loginId;
-	private String auth;
-}
